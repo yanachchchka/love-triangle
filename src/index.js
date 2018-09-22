87332f87bfcd8f9a2fc167a26fbc6c05867f4773
Yana Kachan <yana.kachan94@gmail.com>
2018-09-22 20:32:21 +0300

    lovers_founded

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  var answer = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    var a = i+1;
+    var b = preferences[a-1];
+    var c = preferences[b-1];
+    var d = preferences[c-1];
+
+    if (a==d && a!=b && b!=c){
+      answer++;
+      
+      preferences[a-1] = -1;
+      preferences[b-1] = -1;
+      preferences[c-1] = -1;
+    }
+  }
+  return answer;
 };
